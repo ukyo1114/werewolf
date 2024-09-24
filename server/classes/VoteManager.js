0a60a27c4be835a02793ef03115cf935dadfba31
ukyo1114 <xjr1300sunny@gmail.com>
2024-09-24 14:45:18 +0900

    VoteManager getExecutionTarget追加

--- server/classes/VoteManager.js
+++ server/classes/VoteManager.js
@@ -31,6 +31,29 @@ class VoteManager {
     return voteCount;
   }
 
+  getExecutionTarget(players, phase) {
+    const voteCount = this.voteCounter(phase);
+
+    if (!voteCount) return null;
+
+    let executionTargets = [];
+    let maxVotes = 0;
+
+    voteCount.forEach((count, votee) => {
+      if (count > maxVotes) {
+        executionTargets = [votee];
+        maxVotes = count;
+      }
+      if (count === maxVotes) {
+        executionTargets.push(votee);
+      }
+    });
+
+    const index = Math.floor(Math.random() * executionTargets.length);
+
+    return players.find((pl) => pl._id === executionTargets[index]);
+  }
+
   getVoteHistory(phase) {
     const { currentDay, currentPhase } = phase;
 
