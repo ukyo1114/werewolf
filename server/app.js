1f59942b647286a96e483d96b73c5148402ff97e
ukyo1114 <xjr1300sunny@gmail.com>
2024-09-29 21:48:47 +0900

    gameSelectorリファクタ後

--- server/app.js
+++ server/app.js
@@ -6,11 +6,11 @@ const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const channelRoutes = require("./routes/channelRoutes");
 const messageRoutes = require("./routes/messageRoutes");
+const spectateRoutes = require("./routes/spectateRoutes");
 const blockRoutes = require("./routes/blockRoutes");
 const gameRoutes = require("./routes/gameRoutes");
 const socketHandler = require("./socketHandlers/socketHandler");
 const errorHandler = require("./middleware/errorHandler");
-const { errors } = require("./messages");
 
 const app = express();
 
@@ -29,6 +29,7 @@ app.get("/", (req, res) => {
 app.use("/api/user", userRoutes);
 app.use("/api/channel", channelRoutes);
 app.use("/api/message", messageRoutes);
+app.use("/api/spectate", spectateRoutes);
 app.use("/api/block", blockRoutes);
 app.use("/api/game", gameRoutes);
 
