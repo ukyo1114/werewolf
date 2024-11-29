54acfaf8bbcd77dc789913ca5928f25a1eb8f524
ukyo1114 <xjr1300sunny@gmail.com>
2024-11-29 19:14:09 +0900

    コードスプリッティング試用

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,23 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import { Suspense, lazy } from "react";
 
-import Home from "./pages/Home.jsx";
-import Channels from "./pages/Channels.jsx";
-import { EmailVerification } from "./pages/EmailVerification.jsx";
-import { ResetPassword } from "./pages/ResetPassword.jsx";
+const Home = lazy(() => import("./pages/Home.jsx"));
+const Channels = lazy(() => import("./pages/Channels.jsx"));
+const EmailVerification = lazy(() => import("./pages/EmailVerification.jsx"));
+const ResetPassword = lazy(() => import("./pages/ResetPassword.jsx"));
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chats" element={<Channels />} />
-        <Route path="/verification/:token" element={<EmailVerification />} />
-        <Route path="/reset-password/:token" element={<ResetPassword />} />
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chats" element={<Channels />} />
+          <Route path="/verification/:token" element={<EmailVerification />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
